e04c88a39442088dc408002eb14dbf989d3c319e
Zhaoxianglin <sisivip@vip.qq.com>
2021-06-16 15:37:52 +0800

    hello world

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-        baseURL: "https://formbotserver.herokuapp.com",
+        baseURL: "http://47.242.140.70:8080",
         timeout: 5000,
     })
 
